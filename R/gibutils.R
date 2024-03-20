66e30769088818ec3fbacdaa097ddc1d8c529fd3
gibo <gibo.gaf@gmail.com>
2024-03-20 20:01:52 +0100

    some restyling

--- R/gibutils.R
+++ R/gibutils.R
@@ -6,6 +6,7 @@ combn_l <- function(k) lapply(1:k, function(x) combn(k, m = k - x + 1))
 # combn_l(3)
 
 # Riprendo combn_char dal pacchetto cuber
+
 #' Generate all combinations of the elements of a character vector
 #' 
 #' @param x a character vector
@@ -17,20 +18,23 @@ combn_l <- function(k) lapply(1:k, function(x) combn(k, m = k - x + 1))
 #' combn_char(c("gender", "sector"))
 #' combn_char(c("gender", "sector", "education"))
 #' @export
-combn_char <- function(x){
+combn_char <- function(x) {
   l <- length(x)
   comb_vars <- combn_l(l)
   
   list_comb <- vector(mode = "list", length = l)
-  for(i in seq_along(comb_vars)){
+  
+  for (i in seq_along(comb_vars)) {
     list_comb_i <- vector(mode = "list", length = ncol(comb_vars[[i]]))
     
-    for(j in seq_along(list_comb_i)){
+    for (j in seq_along(list_comb_i)) {
       k <- comb_vars[[i]][ , j]
       list_comb_i[[j]] <- x[k]
     }
+    
     list_comb[[i]] <- list_comb_i
   }
+  
   return(list_comb)
 }
 
@@ -40,7 +44,7 @@ combn_char <- function(x){
 
 
 
-find_factor <- function(.data){
+find_factor <- function(.data) {
   lapply(.data, is.factor)
 }
 # find_factor(x)
@@ -58,34 +62,37 @@ where <- function(f, x) {
 # Ri-creare i factors, aggiungendo "Totale" ai levels salvati in precedenza
 
 # Se factor, estrae i livelli (in una lista?)
-extract_levels <- function(df){
+extract_levels <- function(df) {
   are_fact <- where(is.factor, df)  # Vettore di TRUE/FALSE
   
   l_lev <- lapply(df, levels)
+  
   return(l_lev)
 }
 # extract_levels(d)
 
 
 # Input: df un data frame
-# Output: una lista, i cui elementi sono dei vettori character con i valori unici 
-#         di ogni colonna del data frame df
+# Output: una lista, i cui elementi sono dei vettori character 
+#         con i valori unici di ogni colonna del data frame df
 
 #' Functions to be used in conjunction with 'dcc' family
 #' 
 #' @param df a data frame
 #' 
-#' @return a list whose elements are character vectors of the unique values of each column
+#' @return a list whose elements are character vectors of the 
+#'     unique values of each column
 #' 
 #' @examples 
 #' data("invented_wages")
-#' tmp <- extract_unique(df = invented_wages[ , c("gender", "sector")])
+#' tmp <- extract_unique(df = invented_wages[, c("gender", "sector")])
 #' tmp
 #' str(tmp)
 #' @export
-extract_unique <- function(df){
+extract_unique <- function(df) {
   res <- lapply(df, function(x) unique(as.character((x))))
-  # unique da solo manterrebbe eventuali factor (può comunque tornare utile in caso di altri modi di ordinare...)
+  # unique da solo manterrebbe eventuali factor 
+  # (può comunque tornare utile in caso di altri modi di ordinare...)
   return(res)
 }
 # extract_unique(d[1:3])
@@ -99,13 +106,18 @@ extract_unique <- function(df){
 
 #' @rdname extract_unique
 #' @export
-extract_unique2 <- function(df){
+extract_unique2 <- function(df) {
   are_factors <- where(is.factor, df)
   res <- vector(mode = "list", length = length(are_factors))
   
-  for(i in seq_along(df)){
-    if(are_factors[i]) res[[i]] <- levels(df[[i]]) else res[[i]] <- unique(as.character(df[[i]]))
+  for (i in seq_along(df)) {
+    if (are_factors[i]) {
+      res[[i]] <- levels(df[[i]]) 
+    } else {
+      res[[i]] <- unique(as.character(df[[i]]))
+    }
   }
+  
   return(res)
 }
 # extract_unique2(d[1:3])
@@ -116,7 +128,7 @@ extract_unique2 <- function(df){
 
 #' @rdname extract_unique
 #' @export
-extract_unique3 <- function(df){
+extract_unique3 <- function(df) {
   res <- lapply(df, function(x) sort(unique(as.character((x)))))
   return(res)
 }
@@ -125,33 +137,39 @@ extract_unique3 <- function(df){
 
 #' @rdname extract_unique
 #' @export
-extract_unique4 <- function(df){
+extract_unique4 <- function(df) {
   are_factors <- where(is.factor, df)
   res <- vector(mode = "list", length = length(are_factors))
   
-  for(i in seq_along(df)){
-    if(are_factors[i]) res[[i]] <- levels(df[[i]]) else res[[i]] <- sort(unique(as.character(df[[i]])))
+  for (i in seq_along(df)) {
+    if (are_factors[i]) {
+      res[[i]] <- levels(df[[i]]) 
+    } else {
+      res[[i]] <- sort(unique(as.character(df[[i]])))
+    }
   }
+  
   return(res)
 }
 
 
 #' @rdname extract_unique
 #' @export
-extract_unique5 <- function(df){
+extract_unique5 <- function(df) {
   are_factors <- where(is.factor, df)
   res <- vector(mode = "list", length = length(are_factors))
   
-  for(i in seq_along(df)){
+  for (i in seq_along(df)) {
     
-    if(are_factors[i]){
+    if (are_factors[i]) {
       res[[i]] <- levels(df[[i]])
       res[[i]] <- res[[i]][res[[i]] %in% unique(as.character(df[[i]]))]
-    }else{
+    } else {
       res[[i]] <- sort(unique(as.character(df[[i]])))
     }
     
   }
+  
   return(res)
 }
 
