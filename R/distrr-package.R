628a0d44392fb7db9944cbab20dca2099eec5e05
gibo <gibo.gaf@gmail.com>
2024-03-21 18:05:05 +0100

    selected imports from rlang instead of @import rlang

--- R/distrr-package.R
+++ R/distrr-package.R
@@ -2,10 +2,11 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @import rlang
+#' @importFrom rlang set_names warn caller_env quo as_quosure new_quosure
+#'     parse_quo abort have_name expr_text names2 get_expr get_env sym
+#'     is_formula is_null is_quosure quos
 #' @importFrom dplyr arrange bind_rows filter summarise mutate select 
 #'     group_by n ungroup  mutate_all 
-#'     
 #' @importFrom utils combn
 #' @importFrom stats na.omit setNames quantile
 #' @importFrom tidyr expand complete
