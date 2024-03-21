644c60b5bbb9c57c116230ffd56e0f6308ce30e2
gibo <gibo.gaf@gmail.com>
2024-03-21 17:59:02 +0100

    replaced @import dplyr with @importFrom dplyr ... (selected functions)

--- R/distrr-package.R
+++ R/distrr-package.R
@@ -3,7 +3,9 @@
 
 ## usethis namespace: start
 #' @import rlang
-#' @import dplyr
+#' @importFrom dplyr arrange bind_rows filter summarise mutate select 
+#'     group_by n ungroup  mutate_all 
+#'     
 #' @importFrom utils combn
 #' @importFrom stats na.omit setNames quantile
 #' @importFrom tidyr expand complete
