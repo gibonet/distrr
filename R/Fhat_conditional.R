5f2f691083d370f9585be7e244e9c18f0d01117d
gibo <gibo.gaf@gmail.com>
2024-11-21 23:01:11 +0100

    avoid pipe |>

--- R/Fhat_conditional.R
+++ R/Fhat_conditional.R
@@ -7,19 +7,28 @@
 #' @param weights character vector of length one, indicating the name of the positive numeric column of weights, which will be used in the estimation of the conditional ecdf
 #' @return a data frame, with the variables used to condition, the x variable, and columns wsum (aggregated sum of weights, based on unique values of x) and Fhat (the estimated conditional Fhat). In addition to data frame, the object will be of classes grouped_df, tbl_df and tbl (from package dplyr) 
 #' @examples 
-#' Fhat_conditional_(mtcars,
-#'                  .variables = c("vs", "am"),
-#'                  x = "mpg",
-#'                  weights = "cyl")
+#' Fhat_conditional_(
+#'   mtcars,
+#'   .variables = c("vs", "am"),
+#'   x = "mpg",
+#'   weights = "cyl"
+#' )
 #' @export              
-Fhat_conditional_ <- function(.data, .variables, x, weights){
+Fhat_conditional_ <- function(.data, .variables, x, weights) {
   group_all <- c(.variables, x)
-  .data |>
-    gby_(group_all) |>
-    sumx_(weights) |>
-    stats::na.omit() |>
-    gby_(.variables) |>
-    mutcumx_("wsum")
+  
+  mutcumx_(
+    gby_(
+      stats::na.omit(
+        sumx_(
+          gby_(.data, group_all), 
+          weights
+        ) 
+      ), 
+      .variables
+    ),
+    "wsum"
+  )
 }
 
 # Sembra funzionare
@@ -34,11 +43,15 @@ Fhat_conditional_ <- function(.data, .variables, x, weights){
 #' data(invented_wages)
 #' Fhat_df_(invented_wages, "wage", "sample_weights")
 #' @export
-Fhat_df_ <- function(.data, x, weights){
-  .data |>
-    gby_(x) |>
-    sumx_(weights) |>
-    dplyr::ungroup() |>
-    stats::na.omit() |>
-    mutcumx_("wsum")
+Fhat_df_ <- function(.data, x, weights) {
+  mutcumx_(
+    .data = stats::na.omit(
+      dplyr::ungroup(
+        sumx_(
+          .data = gby_(.data, .variables = x), x = weights
+        )
+      )
+    ),
+    x = "wsum"
+  )
 }
